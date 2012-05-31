5e60d50726c542239eb0077ff354e8911b66f8f8
mfullen <mike@mikefullen.com>
2012-05-30 21:20:10 -0400

    initial project structure

---
+++ pingpongnet/src/main/java/ping/pong/net/App.java
@@ -0,0 +1,13 @@
+package ping.pong.net;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
