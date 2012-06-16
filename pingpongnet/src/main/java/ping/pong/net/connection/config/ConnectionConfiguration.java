937684c04b292938bf62af23e5434fa839222d54
mfullen <mike@mikefullen.com>
2012-06-16 10:22:36 -0400

    cv

--- pingpongnet/src/main/java/ping/pong/net/connection/config/ConnectionConfiguration.java
+++ pingpongnet/src/main/java/ping/pong/net/connection/config/ConnectionConfiguration.java
@@ -9,11 +9,11 @@ public interface ConnectionConfiguration
     /**
      * Default Keystore name for a key on the classpath
      */
-    final String DEFAULT_KEY_STORE = "key.ks";
+    String DEFAULT_KEY_STORE = "key.ks";
     /**
      * Default password for the keystore
      */
-    final String DEFAULT_KEY_STORE_PASSWORD = "pingpong123";
+    String DEFAULT_KEY_STORE_PASSWORD = "pingpong123";
 
     /**
      * Get the port the TCP connection uses
